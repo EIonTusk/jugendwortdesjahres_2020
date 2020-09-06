b682a48b7eef69bd12f062f0f1cdd0566b4a6708
EIonTusk <schaedelhenry@gmail.com>
2020-09-06 23:36:21 +0200

    added proxy function

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import time
 import sys
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
+from proxy_requests import ProxyRequests
+
+# I'm really sorry for this code. It is a mess.
+# But it works and I am very lazy, so I think it will stay like this for a long time.
+# I will work on this, some day.
 
 class Bot():
     def __init__(self):
@@ -11,8 +16,19 @@ class Bot():
                           'Digga/Diggah', 'Cringe', 'Mashallah']
         print('[#] Programmiert von EIonTusk')
         print('[#] Github: https://github.com/EIonTusk')
+        print('----------------------------------------------------------------------')
+        self.proxy = False
+        self.proxy_list = False
         while True:
-            print('----------------------------------------------------------------------')
+            print('[*] Willst du einen Proxyserver benutzen?[Y/n]')
+            i = input('>>> ')
+            if i in ['YES', 'Y', 'y', 'yes', 'ja', 'j', 'JA', 'J']:
+                self.proxy = True
+                break
+            else:
+                print('[!] Ungültige Eingabe!')
+
+        while True:
             print('[*] Gib dein Jugendwort des Jahres aus den Top10 ein.')
             print('[*] Top10: %s'%(self.vote_list))
             self.name = input('>>> ')
@@ -30,7 +46,7 @@ class Bot():
 
         self.trys = 0
         self.success = 0
-        self.url = "https://woerterbuch.langenscheidt.de/js20/top10/vote"
+        self.url = 'https://woerterbuch.langenscheidt.de/js20/top10/vote'
         self.url_site = 'https://woerterbuch.langenscheidt.de/js20/top10'
         self.headers = {'Host': 'woerterbuch.langenscheidt.de',
                         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0',
@@ -44,8 +60,10 @@ class Bot():
                         'Cookie': 'js20=eyJ0b3AxMEFnZUlkeCI6MiwidG9wMTBXb3JkSWR4IjowLCJ0b3AxMENvbnNlbnQiOjEsInZvdGVkVG9wMTAiOjAsInZvdGVkVG9wMTBXb3JkIjoiU2NoYWJlcm5hY2sifQ==; js20.sig=W0UAPnkamc9lOvQRdLP4hJNhdRs',
                         'Upgrade-Insecure-Requests': '1',
                         'TE': 'Trailers'}
+
         self.check = 'Wir haben deine Abstimmung gespeichert.'
         age_list = ['1', '2', '3', '4']
+
         session = HTMLSession()
         r = session.get(self.url_site)
         r.html.render()
@@ -55,6 +73,7 @@ class Bot():
         r = requests.get(iframe)
         soup = BeautifulSoup(r.text, 'html.parser')
         token = soup.find(id='recaptcha-token')['value']
+
         self.data = {'age': random.choice(age_list),
                      'w': str(self.vote),
                      'consent': '1',
@@ -64,8 +83,15 @@ class Bot():
 
     def send(self):
         try:
-            r = requests.post(self.url, data=self.data, headers=self.headers)
-            if r.status_code == 200 and self.check in r.text:
+            if not self.proxy:
+                r = requests.post(self.url, data=self.data, headers=self.headers)
+            elif self.proxy:
+                r = ProxyRequests(self.url)
+                r.set_headers(self.headers)
+                r.post_with_headers(self.data)
+            else:
+                sys.stdout.write('\r[!] Ein Fehler ist aufgetreten                                                      \r\n')
+            if self.check in str(r):
                 self.success += 1
                 sys.stdout.write('\r[+] Für %s abgestimmt                                                           \r\n' %self.name)
             else:
@@ -74,14 +100,12 @@ class Bot():
             sys.stdout.write('\r[!] Ein Fehler ist aufgetreten                                                      \r\n')
         self.trys += 1
 
-
-
     def loop(self):
         while True:
             self.send()
             sys.stdout.write('\rVersuche: %s, Erfolgreiche Versuche: %s, Fehlgeschlagene Versuche: %s' %(self.trys, self.success, self.trys - self.success))
             sys.stdout.flush()
-            time.sleep(random.randint(5, 10))
+            time.sleep(random.randint(3, 5))
 
 if __name__ == '__main__':
     bot = Bot()
